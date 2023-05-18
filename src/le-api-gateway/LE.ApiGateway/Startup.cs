94a3df673183b6e7eaa8cbeacb27de91801102de
Nguyễn Văn Thương <80138135+thuongnguyen1508@users.noreply.github.com>
2023-05-18 22:37:22 +0700

    release alpha version (#1)
    * remove redundant code
    
    * add docker file
    
    * Create cd.yml
    
    * Update Dockerfile
    
    * Update ocelot.global.json
    
    * Update ocelot.global.json
    
    * update consul host
    
    * Update ocelot.global.json
    
    * test cd
    
    * test cd
    
    * test
    
    * test cd
    
    * set up routing for user-service
    
    * Turn off authentication for auth request
    
    * Create ocelot.notification-service.json
    
    * add user-service route
    
    * Update ocelot.notification-service.json
    
    * open route
    
    * test is redundant downstream
    
    * Revert "test is redundant downstream"
    
    This reverts commit c660020099bc9ca9180222726904087e4f3c3ea5.
    
    * refactor code
    
    * Update cd.yml

--- src/le-api-gateway/LE.ApiGateway/Startup.cs
+++ src/le-api-gateway/LE.ApiGateway/Startup.cs
@@ -1,17 +1,12 @@
 using LE.ApiGateway.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LE.ApiGateway
 {
@@ -42,8 +37,6 @@ namespace LE.ApiGateway
             });
 
             services.AddCustomAuthorization(Configuration);
-            services.AddCustomApiVersioning();
-            services.AddSwagger(Configuration);
             services.AddOcelot(Configuration)
                 .AddConsul();
         }
@@ -58,7 +51,7 @@ namespace LE.ApiGateway
             app.UseRouting();
             app.UseCustomAuthorization();
             app.UseHealthCheck("/");
-            app.RunSwaggerUI();
+            //app.RunSwaggerUI();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
             app.UseWebSockets();
